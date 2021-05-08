3e191cf82023e5a7b3f4feb2f29100cfa1f7e26c
zeus64 <loki5100-nospam@yahoo.fr>
2021-05-08 10:48:23 +0200

    Update the patcher thanks to filiptoskovic

--- embarcadero/sydney/10_4_2/update.bat
+++ embarcadero/sydney/10_4_2/update.bat
@@ -1,5 +1,8 @@
 @echo off
 
+FOR /F "usebackq tokens=3*" %%A IN (`reg query "HKCU\Software\Embarcadero\BDS\21.0" /v RootDir`) DO set EmbSourceDir=%%A %%B 
+set EmbSourceDir=%EmbSourceDir:~0,-1%source
+
 FOR %%a IN ("%%~dp0") DO set "ProjectDir=%%~dpa"
 IF %ProjectDir:~-1%==\ SET ProjectDir=%ProjectDir:~0,-1%
 
@@ -16,23 +19,23 @@ mkdir %FileName%\ios
 mkdir %FileName%\android
 mkdir %FileName%\win
 
-echo Copy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\fmx" to "%ProjectDir%\fmx"
-xcopy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\fmx" "%ProjectDir%\fmx"
+echo Copy "%EmbSourceDir%\fmx" to "%ProjectDir%\fmx"
+xcopy "%EmbSourceDir%\fmx" "%ProjectDir%\fmx"
 IF ERRORLEVEL 1 goto ERROR
 echo.
 
-echo Copy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\rtl\ios" to "%ProjectDir%\rtl\ios"
-xcopy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\rtl\ios" "%ProjectDir%\rtl\ios"
+echo Copy "%EmbSourceDir%\rtl\ios" to "%ProjectDir%\rtl\ios"
+xcopy "%EmbSourceDir%\rtl\ios" "%ProjectDir%\rtl\ios"
 IF ERRORLEVEL 1 goto ERROR
 echo.
 
-echo Copy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\rtl\android" to "%ProjectDir%\rtl\android"
-xcopy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\rtl\android" "%ProjectDir%\rtl\android"
+echo Copy "%EmbSourceDir%\rtl\android" to "%ProjectDir%\rtl\android"
+xcopy "%EmbSourceDir%\rtl\android" "%ProjectDir%\rtl\android"
 IF ERRORLEVEL 1 goto ERROR
 echo.
 
-echo Copy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\rtl\win" to "%ProjectDir%\rtl\win"
-xcopy "c:\Program Files (x86)\Embarcadero\Studio\21.0\source\rtl\win" "%ProjectDir%\rtl\win"
+echo Copy "%EmbSourceDir%\rtl\win" to "%ProjectDir%\rtl\win"
+xcopy "%EmbSourceDir%\rtl\win" "%ProjectDir%\rtl\win"
 IF ERRORLEVEL 1 goto ERROR
 echo.
 

b05eea470a4661687fe9d85b2e06bf992f979ae9
zeus64 <loki5100-nospam@yahoo.fr>
2021-09-06 21:53:40 +0200

    add DeployProjNormalizer tool

--- tools/DprojNormalizer/_source/DprojNormalizer.dpr
+++ tools/DprojNormalizer/_source/DprojNormalizer.dpr
@@ -14,7 +14,6 @@ uses
 
 {***********************************************************}
 Procedure _SortAttributesByNodeName(const aNode: TalXmlNode);
-var i: integer;
 Begin
   if ANode = nil then exit;
   if ANode.AttributeNodes <> nil then
@@ -25,14 +24,12 @@ Begin
       end);
   //-----
   if aNode.ChildNodes <> nil then
-    for i := 0 to aNode.ChildNodes.Count - 1 do
+    for var i := 0 to aNode.ChildNodes.Count - 1 do
       _SortAttributesByNodeName(aNode.ChildNodes[i]);
 End;
 
-{***************************************************************}
-Procedure _SortNodesByNameAndAttributes(const aNode: TalXmlNode);
-var LXmlStr1, LXmlStr2: AnsiString;
-    i: integer;
+{************************************************************************}
+Procedure _SortChildNodesByNodeNameAndAttributes(const aNode: TalXmlNode);
 Begin
   if ANode = nil then exit;
   if ANode.ChildNodes <> nil then
@@ -41,6 +38,8 @@ Begin
       begin
         result := ALCompareStr(List[index1].NodeName, List[index2].NodeName);
         if (result = 0) then begin
+          var LXmlStr1: AnsiString;
+          var LXmlStr2: AnsiString;
           List[index1].SaveToXML(LXmlStr1);  // << yes I know it's ugly
           List[index2].SaveToXML(LXmlStr2);  // << but I m lazzy
           result := ALCompareStr(LXmlStr1, LXmlStr2);
@@ -48,17 +47,16 @@ Begin
       end);
   //-----
   if aNode.ChildNodes <> nil then
-    for i := 0 to aNode.ChildNodes.Count - 1 do
-      _SortNodesByNameAndAttributes(aNode.ChildNodes[i]);
+    for var i := 0 to aNode.ChildNodes.Count - 1 do
+      _SortChildNodesByNodeNameAndAttributes(aNode.ChildNodes[i]);
 End;
 
 {*******************************************************************}
 Procedure _RemoveEmptyProjectExtensionsNode(const aNode: TalXmlNode);
-var i: integer;
 Begin
   if ANode = nil then exit;
   if aNode.ChildNodes <> nil then
-    for i := aNode.ChildNodes.Count - 1 downto 0 do
+    for var i := aNode.ChildNodes.Count - 1 downto 0 do
       if (aNode.ChildNodes[i].NodeName = 'ProjectExtensions') and
          ((aNode.ChildNodes[i].ChildNodes = nil) or
           (aNode.ChildNodes[i].ChildNodes.Count = 0)) then
@@ -67,14 +65,11 @@ End;
 
 {***************************************************}
 Procedure _RemoveCDataNodes(const aNode: TalXmlNode);
-var LValue: AnsiString;
-    LParentNode: TalXmlNode;
-    i: integer;
 Begin
   if ANode = nil then exit;
   if aNode.NodeType = ntCData then begin
-    LValue := aNode.Text;
-    LParentNode := ANode.ParentNode;
+    var LValue := aNode.Text;
+    var LParentNode := ANode.ParentNode;
     if LParentNode.ChildNodes.Count <> 1 then
       raise Exception.Create('Error 2F418A94-9F42-4418-90D7-ED8EC120D1B0');
     LParentNode.ChildNodes.Clear;
@@ -82,28 +77,9 @@ Begin
   end;
   //-----
   if aNode.ChildNodes <> nil then
-    for i := 0 to aNode.ChildNodes.Count - 1 do
+    for var i := 0 to aNode.ChildNodes.Count - 1 do
       _RemoveCDataNodes(aNode.ChildNodes[i]);
 End;
-
-var
-  LDProjFilename: AnsiString;
-  LCreateBackup: Boolean;
-  LXmlDoc: TalXmlDocument;
-  LXmlStr: AnsiString;
-  LItemGroupNode: TalXmlNode;
-  LProjectExtensionsNode: TalXmlNode;
-  LDeploymentNode: TalXmlNode;
-  LBorlandProjectNode: TalXmlNode;
-  LDeployFileNode: TalXmlNode;
-  LDelphiCompileNodes: Tarray<TalXmlNode>;
-  LDelphiCompileNode: TalXmlNode;
-  LBuildConfigurationNodes: Tarray<TalXmlNode>;
-  LBuildConfigurationNode: TalXmlNode;
-  LProjectRootNodes: Tarray<TalXmlNode>;
-  LProjectRootNode: TalXmlNode;
-  LEnabledNode: TalXmlNode;
-  I, j: integer;
 
 begin
 
@@ -116,119 +92,123 @@ begin
     SetMultiByteConversionCodePage(CP_UTF8);
 
     //init LDProjFilename / LCreateBackup
-    LDProjFilename := ALTrim(ansiString(paramstr(1)));
-    LCreateBackup := not ALSameText(ALTrim(ansiString(paramstr(2))), 'false');
+    var LDProjFilename := ALTrim(ansiString(paramstr(1)));
+    if LDProjFilename = '' then raise Exception.Create('Usage: DprojNormalizer.exe "<DprojFilename>" <createBackup>(ie: true/false)');
+    var LCreateBackup := not ALSameText(ALTrim(ansiString(paramstr(2))), 'false');
 
-    //create the LXmlDoc
-    LXmlDoc := TALXmlDocument.Create('root');
+    //create the LDProjXmlDoc
+    var LDProjXmlDoc := TALXmlDocument.Create('root');
     Try
 
-      //load the LXmlDoc
-      LXmlDoc.Options := [];
-      LXmlDoc.ParseOptions := [];
-      LXmlDoc.LoadFromFile(LDProjFilename);
+      //load the LDProjXmlDoc
+      LDProjXmlDoc.Options := [];
+      LDProjXmlDoc.ParseOptions := [];
+      LDProjXmlDoc.LoadFromFile(LDProjFilename);
 
       //init LProjectExtensionsNode
-      LProjectExtensionsNode := LXmlDoc.DocumentElement.ChildNodes.FindNode('ProjectExtensions');
+      var LProjectExtensionsNode := LDProjXmlDoc.DocumentElement.ChildNodes.FindNode('ProjectExtensions');
+      if LProjectExtensionsNode = nil then raise Exception.Create('ProjectExtensions node not found!');
 
       //init LBorlandProjectNode
-      LBorlandProjectNode := nil;
-      if LProjectExtensionsNode <> nil then LBorlandProjectNode := LProjectExtensionsNode.ChildNodes.FindNode('BorlandProject');
+      var LBorlandProjectNode := LProjectExtensionsNode.ChildNodes.FindNode('BorlandProject');
+      if LBorlandProjectNode = nil then raise Exception.Create('ProjectExtensions.BorlandProject node not found!');
 
       //init LDeploymentNode
-      LDeploymentNode := nil;
-      if LBorlandProjectNode <> nil then LDeploymentNode := LBorlandProjectNode.ChildNodes.FindNode('Deployment');
+      var LDeploymentNode := LBorlandProjectNode.ChildNodes.FindNode('Deployment');
+      if LDeploymentNode = nil then raise Exception.Create('ProjectExtensions.BorlandProject.Deployment node not found!');
+
+      //check version attribute <Deployment Version="3">
+      if LDeploymentNode.Attributes['Version'] <> '3' then
+        raise Exception.Create('ProjectExtensions.BorlandProject.Deployment.Version is not in the expected value (3)');
 
       //init LItemGroupNode
-      LItemGroupNode := LXmlDoc.DocumentElement.ChildNodes.FindNode('ItemGroup');
+      var LItemGroupNode := LDProjXmlDoc.DocumentElement.ChildNodes.FindNode('ItemGroup');
+      if LItemGroupNode = nil then raise Exception.Create('ItemGroup node not found!');
 
       //order ItemGroup
       _SortAttributesByNodeName(LItemGroupNode);
-      _SortNodesByNameAndAttributes(LItemGroupNode);
+      _SortChildNodesByNodeNameAndAttributes(LItemGroupNode);
 
       //put DelphiCompile at the top (don't know if it's matter)
-      if LItemGroupNode <> nil then begin
-        setlength(LDelphiCompileNodes, 0);
-        while True do begin
-          LDelphiCompileNode := LItemGroupNode.ChildNodes.FindNode('DelphiCompile');
-          if LDelphiCompileNode <> nil then begin
-            Setlength(LDelphiCompileNodes, length(LDelphiCompileNodes)+1);
-            LDelphiCompileNodes[length(LDelphiCompileNodes) - 1] := LItemGroupNode.ChildNodes.Extract(LDelphiCompileNode);
-          end
-          else break;
-        end;
-        for I := High(LDelphiCompileNodes) downto Low(LDelphiCompileNodes) do
-          LItemGroupNode.ChildNodes.insert(0, LDelphiCompileNodes[i]);
+      var LDelphiCompileNodes: Tarray<TalXmlNode>;
+      setlength(LDelphiCompileNodes, 0);
+      while True do begin
+        var LDelphiCompileNode := LItemGroupNode.ChildNodes.FindNode('DelphiCompile');
+        if LDelphiCompileNode <> nil then begin
+          Setlength(LDelphiCompileNodes, length(LDelphiCompileNodes)+1);
+          LDelphiCompileNodes[length(LDelphiCompileNodes) - 1] := LItemGroupNode.ChildNodes.Extract(LDelphiCompileNode);
+        end
+        else break;
       end;
+      for var I := High(LDelphiCompileNodes) downto Low(LDelphiCompileNodes) do
+        LItemGroupNode.ChildNodes.insert(0, LDelphiCompileNodes[i]);
 
       //put BuildConfiguration at the end (don't know if it's matter)
-      if LItemGroupNode <> nil then begin
-        setlength(LBuildConfigurationNodes, 0);
-        while True do begin
-          LBuildConfigurationNode := LItemGroupNode.ChildNodes.FindNode('BuildConfiguration');
-          if LBuildConfigurationNode <> nil then begin
-            Setlength(LBuildConfigurationNodes, length(LBuildConfigurationNodes)+1);
-            LBuildConfigurationNodes[length(LBuildConfigurationNodes) - 1] := LItemGroupNode.ChildNodes.Extract(LBuildConfigurationNode);
-          end
-          else break;
-        end;
-        for I := Low(LBuildConfigurationNodes) to High(LBuildConfigurationNodes) do
-          LItemGroupNode.ChildNodes.add(LBuildConfigurationNodes[i]);
+      var LBuildConfigurationNodes: Tarray<TalXmlNode>;
+      setlength(LBuildConfigurationNodes, 0);
+      while True do begin
+        var LBuildConfigurationNode := LItemGroupNode.ChildNodes.FindNode('BuildConfiguration');
+        if LBuildConfigurationNode <> nil then begin
+          Setlength(LBuildConfigurationNodes, length(LBuildConfigurationNodes)+1);
+          LBuildConfigurationNodes[length(LBuildConfigurationNodes) - 1] := LItemGroupNode.ChildNodes.Extract(LBuildConfigurationNode);
+        end
+        else break;
       end;
+      for var I := Low(LBuildConfigurationNodes) to High(LBuildConfigurationNodes) do
+        LItemGroupNode.ChildNodes.add(LBuildConfigurationNodes[i]);
 
       //order LDeploymentNode
       _SortAttributesByNodeName(LDeploymentNode);
-      _SortNodesByNameAndAttributes(LDeploymentNode);
+      _SortChildNodesByNodeNameAndAttributes(LDeploymentNode);
 
       //remove from deployment unnecessary items
       //https://quality.embarcadero.com/browse/RSP-28003
-      if LDeploymentNode <> nil then
-        for I := LDeploymentNode.ChildNodes.Count - 1 downto 0 do begin
-          LEnabledNode := nil;
-          LDeployFileNode := LDeploymentNode.ChildNodes[i];
-          if (ALSameText(LDeployFileNode.NodeName, 'DeployFile')) then begin
-            for j := LDeployFileNode.ChildNodes.Count - 1 downto 0 do begin
-              LEnabledNode := LDeployFileNode.ChildNodes[j].ChildNodes.FindNode('Enabled');
-              if (LEnabledNode <> nil) and (ALSameText(LEnabledNode.Text,'false')) then break;
-            end;
-            if (not ALSameText(LDeployFileNode.attributes['Class'], 'File')) and
-               ((LEnabledNode = nil) or                                           // normally we can also update other properties of a deploy file not only
-                (not ALSameText(LEnabledNode.Text,'false'))) then                 // enabled, but i consider we can only update enabled
-              LDeploymentNode.ChildNodes.Delete(i);
-          end
-          else if (ALSameText(LDeployFileNode.NodeName, 'DeployClass')) or    // this DeployClass seam not correctly updated
-                  (ALSameText(LDeployFileNode.NodeName, 'ProjectRoot')) then  // so I prefer to delete them (don't know what could be the consequence)
-            LDeploymentNode.ChildNodes.Delete(i);                             // and ProjectRoot seam also to be useless
-        end;
-
-      //put ProjectRoot at the end (don't know if it's matter)
-      if LDeploymentNode <> nil then begin
-        setlength(LProjectRootNodes, 0);
-        while True do begin
-          LProjectRootNode := LDeploymentNode.ChildNodes.FindNode('ProjectRoot');
-          if LProjectRootNode <> nil then begin
-            Setlength(LProjectRootNodes, length(LProjectRootNodes)+1);
-            LProjectRootNodes[length(LProjectRootNodes) - 1] := LDeploymentNode.ChildNodes.Extract(LProjectRootNode);
-          end
-          else break;
-        end;
-        for I := Low(LProjectRootNodes) to High(LProjectRootNodes) do
-          LDeploymentNode.ChildNodes.add(LProjectRootNodes[i]);
+      for var I := LDeploymentNode.ChildNodes.Count - 1 downto 0 do begin
+        var LEnabledNode: TALXmlNode := nil;
+        var LDeployFileNode := LDeploymentNode.ChildNodes[i];
+        if (ALSameText(LDeployFileNode.NodeName, 'DeployFile')) then begin
+          for var j := LDeployFileNode.ChildNodes.Count - 1 downto 0 do begin
+            LEnabledNode := LDeployFileNode.ChildNodes[j].ChildNodes.FindNode('Enabled');
+            if (LEnabledNode <> nil) and (ALSameText(LEnabledNode.Text,'false')) then break;
+          end;
+          if (not ALSameText(LDeployFileNode.attributes['Class'], 'File')) and
+             ((LEnabledNode = nil) or                                           // normally we can also update other properties of a deploy file not only
+              (not ALSameText(LEnabledNode.Text,'false'))) then                 // enabled, but i consider we can only update enabled
+            LDeploymentNode.ChildNodes.Delete(i);
+        end
+        else if (ALSameText(LDeployFileNode.NodeName, 'DeployClass')) or    // this DeployClass seam not correctly updated
+                (ALSameText(LDeployFileNode.NodeName, 'ProjectRoot')) then  // so I prefer to delete them (don't know what could be the consequence)
+          LDeploymentNode.ChildNodes.Delete(i);                             // and ProjectRoot seam also to be useless
       end;
 
+      //put ProjectRoot at the end (don't know if it's matter)
+      var LProjectRootNodes: Tarray<TalXmlNode>;
+      setlength(LProjectRootNodes, 0);
+      while True do begin
+        var LProjectRootNode := LDeploymentNode.ChildNodes.FindNode('ProjectRoot');
+        if LProjectRootNode <> nil then begin
+          Setlength(LProjectRootNodes, length(LProjectRootNodes)+1);
+          LProjectRootNodes[length(LProjectRootNodes) - 1] := LDeploymentNode.ChildNodes.Extract(LProjectRootNode);
+        end
+        else break;
+      end;
+      for var I := Low(LProjectRootNodes) to High(LProjectRootNodes) do
+        LDeploymentNode.ChildNodes.add(LProjectRootNodes[i]);
+
       //Remove Empty ProjectExtensions Node
-      _RemoveEmptyProjectExtensionsNode(LXmlDoc.DocumentElement);
+      _RemoveEmptyProjectExtensionsNode(LDProjXmlDoc.DocumentElement);
 
       //remove CData nodes
-      _RemoveCDataNodes(LXmlDoc.DocumentElement);
+      _RemoveCDataNodes(LDProjXmlDoc.DocumentElement);
 
       //save the file to LXmlStr
-      LXmlDoc.SaveToXML(LXmlStr);
+      var LXmlStr: AnsiString;
+      LDProjXmlDoc.SaveToXML(LXmlStr);
 
       //now add the indent to the node
-      LXmlDoc.Options := [doNodeAutoIndent];
-      LXmlDoc.LoadFromXML(LXmlStr);
-      LXmlDoc.SaveToXML(LXmlStr);
+      LDProjXmlDoc.Options := [doNodeAutoIndent];
+      LDProjXmlDoc.LoadFromXML(LXmlStr);
+      LDProjXmlDoc.SaveToXML(LXmlStr);
 
       //save the dproj
       if LCreateBackup then begin
@@ -238,7 +218,7 @@ begin
       ALSaveStringToFile(cAlUTF8Bom + LXmlStr, LDProjFilename);
 
     Finally
-      ALFreeAndNil(LXmlDoc);
+      ALFreeAndNil(LDProjXmlDoc);
     End;
 
   except
@@ -247,4 +227,5 @@ begin
       ExitCode := 1;
     end;
   end;
+
 end.

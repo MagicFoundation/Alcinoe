0d0aab20defd36f25ce8952590651177be762f78
zeus64 <loki5100-nospam@yahoo.fr>
2022-09-05 19:02:49 +0200

    update case

--- Archive/java/src/com/alcinoe/firebase/dynamiclinks/ALFirebaseDynamicLinksListener.java
+++
@@ -1,6 +0,0 @@
-package com.alcinoe.firebase.dynamiclinks;
-
-public interface ALFirebaseDynamicLinksListener {
-  void onGetDynamicLinkSuccess(final String deepLink, final String invitationId);
-  void onGetDynamicLinkError(final int errorCode, final String errorMsg);
-}
--- Archive/java/src/com/alcinoe/view/inputmethod/ALSoftInputListener.java
+++
@@ -1,6 +0,0 @@
-package com.alcinoe.view.inputmethod;
-
-public interface ALSoftInputListener {
-  void onSoftInputShown();
-  void onSoftInputHidden();
-}

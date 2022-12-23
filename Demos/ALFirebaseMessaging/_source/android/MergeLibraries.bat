3c637828b62482cd7f7251244ba8905cf109ba9b
Zeus <svanderclock@yahoo.fr>
2022-12-23 15:36:34 +0100

    update ALFirebaseMessaging demo

---
+++ Demos/ALFirebaseMessaging/_source/android/MergeLibraries.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set ALBaseDir=%~dp0
+
+"%ALBaseDir%\..\..\..\..\Tools\AndroidMerger\AndroidMerger.exe"^
+ -LocalMavenRepositoryDir="%ALBaseDir%\..\..\..\..\Libraries\jar\"^
+ -Libraries="com.alcinoe:alcinoe-firebase-messaging:1.0.0;%ALBaseDir%\ALFirebaseMessagingApp\"^
+ -OutputDir="%ALBaseDir%\Merged"^
+ -DProj="%ALBaseDir%\..\ALFirebaseMessaging.dproj"^
+ -AndroidManifest="%ALBaseDir%\..\AndroidManifest.template.xml"^
+ -GoogleServicesJson="%ALBaseDir%\GoogleServices\google-services.json"^
+ -DProjNormalizer="%ALBaseDir%\..\..\..\..\Tools\DProjNormalizer\DProjNormalizer.exe"^
+ -RJarSwapper="%ALBaseDir%\..\..\..\..\Tools\RJarSwapper\RJarSwapper.bat"^
+ -UseGradle=true

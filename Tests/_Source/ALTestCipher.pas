a8a12306b669138036209dd72445b6a0c2b83ff7
Zeus <svanderclock@yahoo.fr>
2023-01-11 07:15:40 +0100

    add testing tools

---
+++ Tests/_Source/ALTestCipher.pas
@@ -0,0 +1,95 @@
+unit ALTestCipher;
+
+interface
+
+uses
+  System.Diagnostics,
+  ALCommon,
+  ALString,
+  DUnitX.TestFramework;
+
+type
+
+  [TestFixture]
+  TALTestCipher = class
+  strict private
+    fFullAsciiCharset: TArray<AnsiChar>;
+    fStopWatchAlcinoe: TStopwatch;
+    fStopWatchDELPHI: TStopwatch;
+    procedure CheckExecutionTime(const ARatio: single = 1.2);
+  public
+    constructor create;
+    [Setup]
+    procedure Setup;
+    //[TearDown]
+    //procedure TearDown;
+    [Test]
+    procedure TestALStringHashCrc32;
+  end;
+
+implementation
+
+uses
+  IdHashCRC,
+  IDGlobal,
+  ALCipher;
+
+{*******************************}
+constructor TALTestCipher.create;
+begin
+  Setlength(fFullAsciiCharset, 255);
+  for var I := Low(fFullAsciiCharset) to High(fFullAsciiCharset) do
+    fFullAsciiCharset[i] := AnsiChar(i); // will contain some invalid UTF-8 chars
+end;
+
+{****************************}
+procedure TALTestCipher.Setup;
+begin
+  fStopWatchAlcinoe := TStopwatch.Create;
+  fStopWatchDELPHI := TStopwatch.Create;
+end;
+
+{*********************************************************************}
+procedure TALTestCipher.CheckExecutionTime(const ARatio: single = 1.2);
+begin
+  {$IF defined(debug) or defined(Win32)}
+  //In debug we have overflow checking and range checking so that mean
+  //that the execution time will be much slower that the Delphi RTL so skip it
+  //In Win32 we remove all ASM (fastcode heritage) than the delphi RTL have
+  //so we will be othen much more slower than the Delphi RTL
+  Writeln(ALFormatU('CheckExecutionTime Skipped - %0.0f ms for Alcinoe vs %0.0f ms for Delphi (%0.1fx faster)', [fStopWatchAlcinoe.Elapsed.TotalMilliseconds, fStopWatchDELPHI.Elapsed.TotalMilliseconds, fStopWatchDELPHI.Elapsed.TotalMilliseconds / fStopWatchAlcinoe.Elapsed.TotalMilliseconds], ALDefaultFormatSettingsU));
+  {$ELSE}
+  if fStopWatchAlcinoe.Elapsed.TotalMilliseconds > fStopWatchDELPHI.Elapsed.TotalMilliseconds * ARatio then
+    Assert.Fail(ALFormatU('Time too long (%0.0f ms for Alcinoe vs %0.0f ms for Delphi)', [fStopWatchAlcinoe.Elapsed.TotalMilliseconds, fStopWatchDELPHI.Elapsed.TotalMilliseconds], ALDefaultFormatSettingsU))
+  else
+    //https://github.com/VSoftTechnologies/DUnitX/issues/319
+    Writeln(ALFormatU('%0.0f ms for Alcinoe vs %0.0f ms for Delphi (%0.1fx faster)', [fStopWatchAlcinoe.Elapsed.TotalMilliseconds, fStopWatchDELPHI.Elapsed.TotalMilliseconds, fStopWatchDELPHI.Elapsed.TotalMilliseconds / fStopWatchAlcinoe.Elapsed.TotalMilliseconds], ALDefaultFormatSettingsU));
+  {$ENDIF}
+end;
+
+{********************************************}
+procedure TALTestCipher.TestALStringHashCrc32;
+var LArrayOfByte: TIdBytes;
+begin
+  if not ALTestCrc32cImplementation then
+    Assert.Fail;
+  //--
+  for var I := 0 to 10000 do begin
+    var LStrIn := ALRandomStr(ALRandom32(16384), FFullAsciiCharset);
+    if length(LStrIn) = 0 then continue;
+    var LHash := TIdHashCRC32.Create;
+    try
+      setlength(LArrayOfByte, length(LStrIn));
+      ALmove(pbyte(LStrIn)[low(LStrIn)],LArrayOfByte[0],length(LStrIn));
+      fStopWatchDELPHI.Start; {-} LHash.HashBytesAsHex(LArrayOfByte); {-} fStopWatchDELPHI.Stop;
+    finally
+      LHash.Free;
+    end;
+    //--
+    fStopWatchAlcinoe.Start; {-} ALStringHashCrc32c(LStrIn); {-} fStopWatchAlcinoe.Stop;
+  end;
+  //--
+  CheckExecutionTime(0.04{ARatio});
+end;
+
+end.
---
+++ Tests/_Source/ALTests.dpr
@@ -0,0 +1,77 @@
+program ALTests;
+
+{$IFNDEF TESTINSIGHT}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+{$STRONGLINKTYPES ON}
+uses
+  System.SysUtils,
+  {$IFDEF TESTINSIGHT}
+  TestInsight.DUnitX,
+  {$ELSE}
+  DUnitX.Loggers.Console,
+  DUnitX.Loggers.Xml.NUnit,
+  {$ENDIF }
+  DUnitX.TestFramework,
+  ALTestStrings in 'ALTestStrings.pas',
+  ALTestCipher in 'ALTestCipher.pas';
+
+{$IFNDEF TESTINSIGHT}
+var
+  runner: ITestRunner;
+  results: IRunResults;
+  logger: ITestLogger;
+  nunitLogger : ITestLogger;
+{$ENDIF}
+begin
+
+  {$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+  {$ELSE}
+  ReportMemoryLeaksOnShutdown := False;
+  {$ENDIF}
+  SetMultiByteConversionCodePage(CP_UTF8);
+
+{$IFDEF TESTINSIGHT}
+  TestInsight.DUnitX.RunRegisteredTests;
+{$ELSE}
+  try
+    //Check command line options, will exit if invalid
+    TDUnitX.CheckCommandLine;
+    //Create the test runner
+    runner := TDUnitX.CreateRunner;
+    //Tell the runner to use RTTI to find Fixtures
+    runner.UseRTTI := True;
+    //When true, Assertions must be made during tests;
+    runner.FailsOnNoAsserts := False;
+
+    //tell the runner how we will log things
+    //Log to the console window if desired
+    if TDUnitX.Options.ConsoleMode <> TDunitXConsoleMode.Off then
+    begin
+      logger := TDUnitXConsoleLogger.Create(TDUnitX.Options.ConsoleMode = TDunitXConsoleMode.Quiet);
+      runner.AddLogger(logger);
+    end;
+    //Generate an NUnit compatible XML File
+    nunitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
+    runner.AddLogger(nunitLogger);
+
+    //Run tests
+    results := runner.Execute;
+    if not results.AllPassed then
+      System.ExitCode := EXIT_ERRORS;
+
+    {$IFNDEF CI}
+    //We don't want this happening when running under CI.
+    if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
+    begin
+      System.Write('Done.. press <Enter> key to quit.');
+      System.Readln;
+    end;
+    {$ENDIF}
+  except
+    on E: Exception do
+      System.Writeln(E.ClassName, ': ', E.Message);
+  end;
+{$ENDIF}
+end.

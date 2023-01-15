65bd5c5016b1ff5c4a44e15c45e0bc0fa461bab9
Zeus <svanderclock@yahoo.fr>
2023-01-15 01:28:50 +0100

    update demos

--- Demos/ALConfetti/_source/Android/MergeLibraries.bat
+++ Demos/ALConfetti/_source/Android/MergeLibraries.bat
@@ -4,7 +4,7 @@ set ALBaseDir=%~dp0
 
 "%ALBaseDir%\..\..\..\..\Tools\AndroidMerger\AndroidMerger.exe"^
  -LocalMavenRepositoryDir="%ALBaseDir%\..\..\..\..\Libraries\jar\"^
- -Libraries="androidx.appcompat:appcompat:1.5.1;%ALBaseDir%\ALConfettiDemoApp\"^
+ -Libraries="%ALBaseDir%\ALConfettiDemoApp\"^
  -OutputDir="%ALBaseDir%\Merged"^
  -DProj="%ALBaseDir%\..\ALConfettiDemo.dproj"^
  -AndroidManifest="%ALBaseDir%\..\AndroidManifest.template.xml"^

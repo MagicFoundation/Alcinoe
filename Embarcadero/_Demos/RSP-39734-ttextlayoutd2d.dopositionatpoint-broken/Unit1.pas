a935d78e4b1364ffb9e31915e03f1bcd4a0aea65
Zeus <svanderclock@yahoo.fr>
2024-02-15 13:09:39 +0100

    https://github.com/MagicFoundation/Alcinoe/issues/318

--- Embarcadero/_Demos/RSP-39734-ttextlayoutd2d.dopositionatpoint-broken/Unit1.pas
+++ Embarcadero/_Demos/RSP-39734-ttextlayoutd2d.dopositionatpoint-broken/Unit1.pas
@@ -52,17 +52,15 @@ begin
     showmessage('MeasuredWidth: ' + floattostr(LMeasuredWidth));
     showmessage('PositionAtPoint(x:'+floattostr(LMeasuredWidth - Tepsilon.Position) + '| y:0): ' + inttostr(LLayout.PositionAtPoint(TpointF.Create(LMeasuredWidth - Tepsilon.Position,0))));
     showmessage('PositionAtPoint must return 1 (zero based)');
-    showmessage('''
-      The design of PositionAtPoint is not optimal, as it appears to be specifically
-      tailored to work with TEdit and its text selection mechanism. In its current
-      implementation, PositionAtPoint modifies the returned text position by incrementing
-      it by one when the point is at the trailing edge of a character. This approach
-      might be suitable for the specific behavior expected in TEdit, but it could be limiting
-      or inappropriate for other use cases where a different text interaction model is
-      required. This design choice in PositionAtPoint makes it less flexible and potentially
-      less useful in a broader range of applications beyond its intended integration
-      with TEdit
-      ''');
+    showmessage('The design of PositionAtPoint is not optimal, as it appears to be specifically ' +
+      'tailored to work with TEdit and its text selection mechanism. In its current ' +
+      'implementation, PositionAtPoint modifies the returned text position by incrementing ' +
+      'it by one when the point is at the trailing edge of a character. This approach ' +
+      'might be suitable for the specific behavior expected in TEdit, but it could be limiting ' +
+      'or inappropriate for other use cases where a different text interaction model is ' +
+      'required. This design choice in PositionAtPoint makes it less flexible and potentially ' +
+      'less useful in a broader range of applications beyond its intended integration ' +
+      'with TEdit');
   finally
     LLayout.Free;
   end;
